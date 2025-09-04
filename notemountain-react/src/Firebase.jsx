82bc213070a539abe4aae2eeb9c1521af37643bf
shreeram800 <shreeramdubey800@gmail.com>
2025-09-04 23:29:26 +0530

    added project

---
+++ notemountain-react/src/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_oTpaEYRJE0Xo7m7v9g5iQ21jiT3YBxU",
+  authDomain: "mountainnoteproject.firebaseapp.com",
+  projectId: "mountainnoteproject",
+  storageBucket: "mountainnoteproject.firebasestorage.app",
+  messagingSenderId: "1050052548797",
+  appId: "1:1050052548797:web:3d01b0e1b492d6fdb1ffd4",
+  measurementId: "G-GYGE5RJ9FD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
